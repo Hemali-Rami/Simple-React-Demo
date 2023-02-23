81a0fae2c484e6a375fd42ca0ea187925b9c9847
hemali51 <hemali.r@applocum.org>
2023-02-23 13:38:18 +0530

    review section done

--- src/compoents/FunctionalComponents/Review/Reviewdata.js
+++ src/compoents/FunctionalComponents/Review/Reviewdata.js
@@ -9,7 +9,7 @@ export const people = [
   },
   {
     id: 2,
-    img: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp",
+    img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     name: "Maria Kate",
     desi: "Photographer",
     review:
@@ -17,9 +17,25 @@ export const people = [
   },
   {
     id: 3,
-    img: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp",
+    img: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     name: "Anna Deynah",
     desi: "UX Designer",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit doloremque.",
+  },
+  {
+    id: 4,
+    img: "https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp",
+    name: "Julian Wan",
+    desi: "julianwan",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit doloremque.",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    name: "Christopher Campbell",
+    desi: "HR Manager",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus et deleniti nesciunt sint eligendi reprehenderit reiciendis, quibusdam illo, beatae quia fugit consequatur laudantium velit magnam error. Consectetur distinctio fugit doloremque.",
   },
--- src/compoents/FunctionalComponents/Review/Reviewmain.js
+++ src/compoents/FunctionalComponents/Review/Reviewmain.js
@@ -1,9 +1,44 @@
 import React, { useState } from "react";
 import { people } from "./Reviewdata";
 import "../../../scss/Review.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeft,
+  faArrowRight,
+  faQuoteLeft,
+} from "@fortawesome/fontawesome-free-solid";
+
 export const Reviewmain = () => {
   const [id, setId] = useState(0);
   const { img, name, desi, review } = people[id];
+  const checknumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  }
+  const prevperson = () => {
+    setId((index) => {
+      let newId = index - 1;
+      return checknumber(newId);
+    });
+  };
+  const nextperson = () => {
+    setId((index) => {
+      let newId = index + 1;
+      return checknumber(newId);
+    });
+  };
+  const randomreview = () => {
+    let newrandom = Math.floor(Math.random() * people.length);
+    if (newrandom === id) {
+      newrandom = id + 1;
+    }
+    setId(checknumber(newrandom));
+  };
   return (
     <div className="reviewmain">
       <div className="container">
@@ -11,11 +46,29 @@ export const Reviewmain = () => {
           <h3>Testimonial</h3>
           <div className="reviewboxwrp">
             <div className="testimaonialbox">
-              <img src={img} alt={name} />
+              <div className="imagebox">
+                <img src={img} alt={name} />
+                <FontAwesomeIcon icon={faQuoteLeft} />
+              </div>
               <div className="personpro">
                 <h4>{name}</h4>
                 <span>{desi}</span>
                 <p>{review}</p>
+              </div>
+            </div>
+            <div className="sliderarrows">
+              <div className="slider_arrow next">
+                <button onClick={nextperson}>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+              </div>
+              <div className="slider_arrow previous">
+                <button onClick={prevperson}>
+                  <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+              </div>
+              <div className="randomperson">
+                <button onClick={randomreview}>Random review</button>
               </div>
             </div>
           </div>
